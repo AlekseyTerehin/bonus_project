e6a8df33962d448b1880bbc878e69eeabf8052c0
aleksey_terehin <god_of_forecasers@mail.ru>
2024-02-05 23:33:12 +0300

    optimization of obtaining a list of users

--- backend/users/dto/user_bonus_dto.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from datetime import datetime
-
-
-@dataclass
-class UserBonusDTO:
-    pk: int
-    date_joined: datetime
-    bonuses: list
--- backend/users/repositories/user_queries.py
+++ backend/users/repositories/user_queries.py
@@ -1,10 +1,6 @@
-from itertools import groupby
-from typing import List
-
 from django.contrib.auth.models import User
 from django.db.models import QuerySet
-
-from users.dto.user_bonus_dto import UserBonusDTO
+from django.contrib.postgres.aggregates import ArrayAgg
 
 
 class UserQueries:
@@ -12,22 +8,9 @@ class UserQueries:
     def __init__(self):
         self.users = User.objects.all()
 
-    def get_users(self):
-        users_info = self.users.select_related('userbonus__bonus').values(
-            'pk', 'date_joined', 'userbonus__bonus__bonus_name'
-        ).order_by('pk')
-        return self.__grouped_bonus(users_info=users_info)
-
-    @staticmethod
-    def __grouped_bonus(users_info: QuerySet[User]) -> List[UserBonusDTO]:
-        group_users = groupby(users_info, key=lambda x: (x['pk'], x['date_joined']))
-        users_list = []
-        for group, rows in group_users:
-            user_id, date_joined = group
-            user_bonuses = UserBonusDTO(
-                pk=user_id,
-                date_joined=date_joined,
-                bonuses=[row['userbonus__bonus__bonus_name'] for row in rows]
-            )
-            users_list.append(user_bonuses)
-        return users_list
+    def get_users(self) -> QuerySet[User]:
+        return self.users.values(
+            'pk', 'date_joined'
+        ).annotate(
+            bonuses=ArrayAgg('userbonus__bonus__bonus_name')
+        )
